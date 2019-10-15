72d67fb601c78c35ce7ca319d8695c35d1e673bd
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-10-14 18:26:21 -0700

    use async for auton. Beginning testing once everything is reattached to the robot

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,12 +1,36 @@
 #include "main.h"
 #include "motors.h"
+#include "okapi/api.hpp"
 
-/*
- * The autonomous should do the following:
- * 1. Lift up to full then back down.
- * 2. Drop arms.
-*/
+using namespace okapi;
+
+const double rollkP = 0.001;
+const double rollkI = 0.0001;
+const double rollkD = 0.0001;
+
+auto dt = ChassisControllerFactory::create(
+  L_DR, R_DR,
+  AbstractMotor::gearset::green,
+  {4_in, 15.9375_in}
+);
+
+auto roller_l = AsyncControllerFactory::posPID(L_ROLL, rollkP, rollkI, rollkD);
+auto roller_r = AsyncControllerFactory::posPID(R_ROLL, rollkP, rollkI, rollkD);
+
 
 void autonomous() {
-
+  //Algorithm :
+  //move forward 42.9_in
+  dt.moveDistanceAsync(42.9_cm);
+  roller_l.setTarget(300);
+  roller_r.setTarget(300);
+  dt.waitUntilSettled();
+  //turn left 90º
+  dt.turnAngle(90_deg);
+  //move forward 19.4_in
+  dt.moveDistance(19.4_in);
+  //turn left 90º
+  dt.turnAngle(90_deg);
+  //move forward 30.7_in
+  dt.moveDistance(30.7_in);
 }
