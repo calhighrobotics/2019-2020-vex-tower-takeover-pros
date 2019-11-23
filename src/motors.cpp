da45523219d3ac6b91e91c2be29737c163413914
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-11-23 10:56:32 -0800

    uncomment motors.cpp

--- src/motors.cpp
+++ src/motors.cpp
@@ -1,9 +1,9 @@
 #include "motors.hpp"
 
-// pros::Motor push(PUSH);
-// pros::Motor dr_l(L_DR, /* reversed */ true);
-// pros::Motor dr_r(R_DR);
-// pros::Motor roller_left(L_ROLL);
-// pros::Motor roller_right(R_ROLL, /* reversed */ true);
-// pros::Motor arm_l(L_ARM);
-// pros::Motor arm_r(R_ARM, /* reversed */ true);
+pros::Motor push(PUSH);
+pros::Motor dr_l(L_DR, /* reversed */ true);
+pros::Motor dr_r(R_DR);
+pros::Motor roller_left(L_ROLL);
+pros::Motor roller_right(R_ROLL, /* reversed */ true);
+pros::Motor arm_l(L_ARM);
+pros::Motor arm_r(R_ARM, /* reversed */ true);
