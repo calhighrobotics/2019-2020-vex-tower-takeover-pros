5e8bf0292dfe6dd136641a3d388fe2e0bda6dc42
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-11-23 10:55:34 -0800

    add temporary autonomous program.
    this should just move the arms out and make it easier to start driver control. it also releases the grenade pin.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,30 +1,12 @@
 #include "main.hpp"
 #include "motors.hpp"
-#include "okapi/api.hpp"
-
-using namespace okapi;
-
-const double rollkP = 0.001;
-const double rollkI = 0.0001;
-const double rollkD = 0.0001;
-
-auto dt = ChassisControllerFactory::create(
-  L_DR, R_DR,
-  AbstractMotor::gearset::green,
-  {4_in, 15.9375_in}
-);
 
 void autonomous() {
-  //Algorithm :
-  //move forward 42.9_in
-  dt.moveDistanceAsync(42.9_in);
-  dt.waitUntilSettled();
-  //turn left 90º
-  dt.turnAngle(90_deg);
-  //move forward 19.4_in
-  dt.moveDistance(19.4_in);
-  //turn left 90º
-  dt.turnAngle(90_deg);
-  //move forward 30.7_in
-  dt.moveDistance(30.7_in);
+  arm_l.move(127);
+  arm_r.move(127);
+  //wait
+  pros::delay(1000);
+  //stop motors
+  arm_l.move(0);
+  arm_r.move(0);
 }
