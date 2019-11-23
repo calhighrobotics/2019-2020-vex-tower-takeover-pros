2475f5a00b32ca7e745947b89dac105a82a901bc
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-11-23 10:52:20 -0800

    new autonomous

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -4,27 +4,45 @@
 
 using namespace okapi;
 
-const double rollkP = 0.001;
-const double rollkI = 0.0001;
-const double rollkD = 0.0001;
-
 auto dt = ChassisControllerFactory::create(
   L_DR, R_DR,
   AbstractMotor::gearset::green,
   {4_in, 15.9375_in}
 );
 
+const double rollkP = 0.001;
+const double rollkI = 0.0001;
+const double rollkD = 0.0001;
+
+auto left_arm = AsyncControllerFactory::posIntegrated(L_ARM);
+auto right_arm = AsyncControllerFactory::posIntegrated(R_ARM);
+
 void autonomous() {
-  //Algorithm :
-  //move forward 42.9_in
+  // arm_l.move(127);
+  // arm_r.move(127);
+  // //wait
+  // pros::delay(1000);
+  // //stop motors
+  // arm_l.move(0);
+  // arm_r.move(0);
+
+  left_arm.setTarget(300);
+  right_arm.setTarget(300);
   dt.moveDistanceAsync(42.9_in);
+  roller_left.move(127);
+  roller_right.move(127);
+  dt.waitUntilSettled();
+  //turn left 90º
+  dt.turnAngleAsync(90_deg);
+  dt.waitUntilSettled();
+  roller_left.move(0);
+  roller_right.move(0);
+  //move forward 19.4_in
+  dt.moveDistance(19.4_in);
   dt.waitUntilSettled();
   //turn left 90º
   dt.turnAngle(90_deg);
-  //move forward 19.4_in
-  dt.moveDistance(19.4_in);
-  //turn left 90º
-  dt.turnAngle(90_deg);
+  dt.waitUntilSettled();
   //move forward 30.7_in
   dt.moveDistance(30.7_in);
 }
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,33 +1,9 @@
 #include "main.hpp"
-// #include "motors.hpp"
+#include "motors.hpp"
 #include <tr1/math.h>
-
-#define PUSH 18
-#define L_DR 13
-#define R_DR 20
-#define L_ROLL 1
-#define R_ROLL 8
-#define L_ARM 11
-#define R_ARM 19
-
-extern pros::Motor push;
-extern pros::Motor dr_l;
-extern pros::Motor dr_r;
-extern pros::Motor roller_left;
-extern pros::Motor roller_right;
-extern pros::Motor arm_l;
-extern pros::Motor arm_r;
 
 void opcontrol() {
   pros::Controller master(CONTROLLER_MASTER);
-
-  pros::Motor push(PUSH);
-  pros::Motor dr_l(L_DR, /* reversed */ true);
-  pros::Motor dr_r(R_DR);
-  pros::Motor roller_left(L_ROLL);
-  pros::Motor roller_right(R_ROLL, /* reversed */ true);
-  pros::Motor arm_l(L_ARM);
-  pros::Motor arm_r(R_ARM, /* reversed */ true);
 
   while (true) {
 
@@ -36,8 +12,8 @@ void opcontrol() {
     dr_r.move(master.get_analog(ANALOG_LEFT_Y) * 5);
 
     //armsn
-    arm_l.move(master.get_analog(ANALOG_RIGHT_Y));
-    arm_r.move(master.get_analog(ANALOG_RIGHT_Y));
+    arm_l.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
+    arm_r.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
 
     //point turn algo
     if (master.get_digital(DIGITAL_R1)) {
@@ -50,23 +26,23 @@ void opcontrol() {
 
     //pushing mechanism
     if (master.get_digital(DIGITAL_R2)) {
-      push.move_velocity(150);
+      push.move_velocity(100);
     } else if (master.get_digital(DIGITAL_L2)) {
-      push.move_velocity(-150);
+      push.move_velocity(-100);
     } else {
       push.move_velocity(0);
     }
 
     //rollers
-    if(master.get_digital(DIGITAL_A)){
+    if(master.get_digital(DIGITAL_A)){// spin in
       roller_right.move(127);
       roller_left.move(127);
-    } else if(master.get_digital(DIGITAL_B)){
-      roller_left.move(-127);
-      roller_right.move(-127);
-    } else {
-      roller_left.move_velocity(0);
-      roller_right.move_velocity(0);
+    } else if(master.get_digital(DIGITAL_B)){ // spin out
+      roller_left.move((3 * -127)/4);
+      roller_right.move((3 * -127)/4);
+    } else { // STOP! in the name of the law!
+      roller_right.move(0);
+      roller_left.move(0);
     }
 
     pros::delay(20);
