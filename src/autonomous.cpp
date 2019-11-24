a7532231fd793fb10e2f784f249f16ea65c8801c
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-11-23 20:54:37 -0800

    push cube into goal auton
    developed @ ceres competition gang gang

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,6 +2,24 @@
 #include "motors.hpp"
 
 void autonomous() {
+  dr_l.move(127);
+  dr_r.move(127);
+  //wait 1 second
+  pros::delay(500);
+  //stop motors
+  dr_l.move(0);
+  dr_r.move(0);
+  //wait 1/2 second
+  pros::delay(500);
+  //go backwards
+  dr_l.move(-127);
+  dr_r.move(-127);
+  //we like to move it move it
+  pros::delay(700);
+  //STOP!
+  dr_l.move(0);
+  dr_r.move(0);
+  //get ready for driver control
   arm_l.move(127);
   arm_r.move(127);
   //wait 1 second
