2257fa48ae8acb934c3c6a1e0c1d34dc89954467
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-12-22 16:41:23 -0800

    im not sure what i did honeslty

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -6,7 +6,7 @@ void opcontrol() {
   pros::Controller master(CONTROLLER_MASTER);
 
   while (true) {
-  
+
     //driving
     dr_l.move(master.get_analog(ANALOG_LEFT_Y) * 5);
     dr_r.move(master.get_analog(ANALOG_LEFT_Y) * 5);
@@ -34,16 +34,15 @@ void opcontrol() {
     }
 
     //rollers
-    if(master.get_digital(DIGITAL_A)){
-      roller_right.move(127);
-      roller_left.move(127);
-      printf("%f\n", roller_left.get_position());
-    } else if(master.get_digital(DIGITAL_B)){
-      roller_left.move(-127);
-      roller_right.move(-127);
+    if(master.get_digital(DIGITAL_A)){// spin in
+      roller_left.move_velocity(600);
+      roller_right.move_velocity(600);
+    } else if(master.get_digital(DIGITAL_B)){ // spin out
+      roller_left.move_velocity(-450);
+      roller_right.move_velocity(-450);
     } else {
-      roller_left.move_velocity(0);
-      roller_right.move_velocity(0);
+      roller_right.move(0);
+      roller_left.move(0);
     }
 
     pros::delay(20);
