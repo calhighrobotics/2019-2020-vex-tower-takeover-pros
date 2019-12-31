7339c2e2513256406327a8f569d5c99c04779c5a
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-12-31 15:49:39 -0800

    switch pusher direction to reflect change in motor orientation

--- src/motors.cpp
+++ src/motors.cpp
@@ -1,6 +1,6 @@
 #include "motors.hpp"
 
-pros::Motor push(PUSH);
+pros::Motor push(-PUSH);
 pros::Motor dr_l(abs(L_DR), /* reversed */ true); //for okapi to work, I need to set a negative port number. however v5 smart doesn't like negative port numbers therefore absolute value needed here.
 pros::Motor dr_r(R_DR);
 pros::Motor roller_left(L_ROLL);
