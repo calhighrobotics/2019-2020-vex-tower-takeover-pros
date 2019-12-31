b528557535dc7486a96b45122c38fe4d188d650e
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2019-12-31 13:07:31 -0800

    upgrade to okapilib v4.0.0

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -45,40 +45,5 @@ void opcontrol() {
       roller_right.move(0);
       roller_left.move(0);
     }
-  //run rollers
-    roller_left.move_velocity(600);
-    roller_right.move_velocity(600);
-  //moving
-    dt.moveDistanceAsync(46_in);
-    dt.waitUntilSettled();
-    pros::delay(400);
-  //stop rollers
-    roller_left.move(0);
-    roller_right.move(0);
-  //move back and speed up
-    dt.setMaxVelocity(180);
-    dt.moveDistanceAsync(-20_in);
-    dt.waitUntilSettled();
-  //turn
-    dt.turnAngleAsync(-103_deg);
-    dt.waitUntilSettled();
-  //move forward to goal
-    dt.moveDistance(22_in);
-    dt.waitUntilSettled();
-  //push up
-    push.move(127);
-    pros::delay(2725);
-    push.move(0);
-  //roll out and pull arms back
-    roller_left.move(-127);
-    roller_right.move(127);
-    arm_r.move(-127);
-    arm_l.move(-127);
-    pros::delay(800);
-    roller_left.move(0);
-    roller_right.move(0);
-    arm_r.move(0);
-    arm_l.move(0);
-  //move back
-    dt.moveDistance(-3_in);
+  }
 }
