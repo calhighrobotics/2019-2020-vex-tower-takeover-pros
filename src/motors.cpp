375e34b57e0c36f1510c2be0220e3d9e7942cfe5
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-01-01 17:03:39 -0800

    changed the way the controls work for driving around and the way the pusher is initialized

--- src/motors.cpp
+++ src/motors.cpp
@@ -1,7 +1,7 @@
 #include "motors.hpp"
 
-pros::Motor push(-PUSH);
-pros::Motor dr_l(abs(L_DR), /* reversed */ true); //for okapi to work, I need to set a negative port number. however v5 smart doesn't like negative port numbers therefore absolute value needed here.
+pros::Motor push(PUSH, /* reversed */ true);
+pros::Motor dr_l(abs(L_DR), /* reversed */ true);
 pros::Motor dr_r(R_DR);
 pros::Motor roller_left(L_ROLL);
 pros::Motor roller_right(abs(R_ROLL), /* reversed */ true);
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -9,27 +9,29 @@ void opcontrol() {
   while (true) {
 
     //driving
-    dr_l.move(master.get_analog(ANALOG_LEFT_Y) * 5);
-    dr_r.move(master.get_analog(ANALOG_LEFT_Y) * 5);
+    if(abs(master.get_analog(ANALOG_LEFT_Y)) > 50){
+      dr_l.move(master.get_analog(ANALOG_LEFT_Y) * 5);
+      dr_r.move(master.get_analog(ANALOG_LEFT_Y) * 5);
+    } else if(master.get_digital(DIGITAL_L1)){
+      dr_r.move_velocity(-600);
+      dr_l.move_velocity(600);
+    } else if (master.get_digital(DIGITAL_R1)){
+      dr_l.move_velocity(-600);
+      dr_r.move_velocity(600);
+    } else {
+      dr_r.move(0);
+      dr_l.move(0);
+    }
 
     //arms
     arm_l.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
     arm_r.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
 
-    //point turn algo
-    if (master.get_digital(DIGITAL_R1)) {
-      dr_l.move_velocity(200);
-      dr_r.move_velocity(-200);
-    } else if (master.get_digital(DIGITAL_L1)) {
-      dr_l.move_velocity(-200);
-      dr_r.move_velocity(200);
-    }
-
     //pushing mechanism
     if (master.get_digital(DIGITAL_R2)) {
-      push.move_velocity(100);
+      push.move(127);
     } else if (master.get_digital(DIGITAL_L2)) {
-      push.move_velocity(-100);
+      push.move(-127);
     } else {
       push.move_velocity(0);
     }
