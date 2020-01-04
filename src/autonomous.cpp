8c9bc687b965ea4a5f1708038e7afdf55035d4a6
noel <zknoel@gmail.com>
2020-01-04 11:06:55 -0800

    Update autonomous.cpp
    Co-Authored-By: Rahul S. Deshpande <dishpanda@users.noreply.github.com>

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -21,22 +21,19 @@ ADIButton right_lim(RIGHT_LIM);
 //bring out arms
   arm_l.move(127);
   arm_r.move(127);
-  pros::delay(800);
+  pros::delay(600);
   arm_r.move(0);
   arm_l.move(0);
 //run rollers
   roller_left.move(600);
   roller_right.move(600);
-// allow preload to enter
-  pros::delay(200);
 //moving
-// dt.setMaxVelocity(35);
 dt.setMaxVelocity(45);
-  dt.moveDistanceAsync(14_in);
+  dt.moveDistanceAsync(48_in);
   dt.waitUntilSettled();
-// dt.setMaxVelocity(45);
-  dt.moveDistanceAsync(33_in);
-  dt.waitUntilSettled();
+//stop rollers
+  roller_left.move(0);
+  roller_right.move(0);
   // pros::delay(200);
 //move back and speed up
   dt.setMaxVelocity(170);
@@ -49,12 +46,15 @@ dt.setMaxVelocity(45);
 //move forward to goal
 dt.setMaxVelocity(180);
   dt.moveDistanceAsync(20_in);
-  //stop rollers
-    roller_left.move(0);
-    roller_right.move(0);
   dt.waitUntilSettled();
+//spit out cubes just a little
+  roller_left.move_velocity(-200);
+  roller_right.move_velocity(200);
+  pros::delay(200);
+  roller_left.move_velocity(0);
+  roller_right.move_velocity(0);
 //push up
-  push.move(90); //went the wrong way!!
+  push.move(90); //went the wrong way!! 
   pros::delay(2000);
   push.move(0);
 //roll out and pull arms back
