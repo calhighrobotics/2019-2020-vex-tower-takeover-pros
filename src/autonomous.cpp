33687badf19d0efef00ac32dfae7716a753019be
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-01-11 21:04:03 -0800

    introduce "slower" toggle mechanic.
    If the "slower" toggle is activated, the robot becomes half as fast (slows by 50%). This allows us to be more precise when we need to be.
    
    also, the auton is the small red.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,10 +1,12 @@
+//SMALL RED GOAL
+
 #include "main.hpp"
 #include "motors.hpp"
 #include "okapi/api.hpp"
 
 using namespace okapi;
 
-void autonomous(){ //SMALL RED/BLUE GOALS
+void autonomous(){ //SMALL RED GOAL
 //local declaration of drive train
 auto drive = ChassisControllerBuilder()
     .withMotors(L_DR, R_DR)
@@ -35,15 +37,14 @@ drive->setMaxVelocity(45);
   pros::delay(150);
 //turn
   drive->turnAngleAsync(107_deg); // RED
-  // drive->turnAngleAsync(-107_deg); // BLUE
   drive->waitUntilSettled();
 //move forward to goal
 drive->setMaxVelocity(180);
-  drive->moveDistanceAsync(23_in);
+  drive->moveDistanceAsync(19.5_in);
   drive->waitUntilSettled();
 //push up
-  push.move(90); //went the wrong way!!
-  pros::delay(2000);
+  push.move(80); //went the wrong way!!
+  pros::delay(1975);
   push.move(0);
 //roll out and pull arms back
   roller_left.move_velocity(-200);
@@ -58,57 +59,3 @@ drive->setMaxVelocity(180);
 //move back
   drive->moveDistance(-10_in);
 }
-
-// void autonomous() { BIG BLUE
-// //local declaration of drive train
-// auto drive = ChassisControllerBuilder()
-//     .withMotors(L_DR, R_DR)
-//     // Green gearset, 4 in wheel diam, 11.5 in wheel track
-//     .withDimensions(AbstractMotor::gearset::green, {{4_in, 15.9375_in}, imev5GreenTPR})
-//     .build();
-// //bring out arms
-//   arm_l.move(127);
-//   arm_r.move(127);
-//   pros::delay(200);
-//   roller_left.move(600);
-//   roller_right.move(600);
-//   pros::delay(400);
-//   arm_r.move(0);
-//   arm_l.move(0);
-// //run rollers
-//
-// //moving
-// drive->setMaxVelocity(45);
-//   drive->moveDistanceAsync(48_in);
-//   drive->waitUntilSettled();
-// //move back and speed up
-//   drive->setMaxVelocity(170);
-// //turn
-//   drive->turnAngleAsync(103_deg); // BLUE
-//   // drive->turnAngleAsync(-103_deg); // RED
-//   drive->waitUntilSettled();
-// //move forward to goal
-// drive->setMaxVelocity(180);
-//   drive->moveDistanceAsync(48_in);
-//   pros::delay(1550);
-//   roller_left.move(0);
-//   roller_right.move(0);
-//   drive->waitUntilSettled();
-//
-// //push up
-//   push.move(90);
-//   pros::delay(1950);
-//   push.move(0);
-// //roll out and pull arms back
-//   roller_left.move_velocity(-200);
-//   roller_right.move_velocity(200);
-//   arm_l.move_velocity(-127);
-//   arm_r.move_velocity(-127);
-//   pros::delay(800);
-//   roller_left.move(0);
-//   roller_right.move(0);
-//   arm_r.move(0);
-//   arm_l.move(0);
-// //move back
-//   drive->moveDistance(-10_in);
-// }
--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -5,46 +5,97 @@
 pros::Controller master(CONTROLLER_MASTER);
 
 void opcontrol(){
+  bool slower = false;
+
   while (true) {
 
-    //driving
-    if(abs(master.get_analog(ANALOG_LEFT_Y)) > 10){
-      dr_l.move(master.get_analog(ANALOG_LEFT_Y) * 5);
-      dr_r.move(master.get_analog(ANALOG_LEFT_Y) * 5);
-    } else if(master.get_digital(DIGITAL_L1)){
-      dr_r.move_voltage(MAX_MOTOR_VOLTAGE);
-      dr_l.move(-MAX_MOTOR_VOLTAGE);
-    } else if (master.get_digital(DIGITAL_R1)){
-      dr_l.move(127);
-      dr_r.move(-127);
-    } else {
-      dr_r.move(0);
-      dr_l.move(0);
+    if(master.get_digital(DIGITAL_RIGHT)){
+      slower = !slower;
     }
 
-    //arms
-    arm_l.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
-    arm_r.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
+    if(slower){
+      //driving
+      if(abs(master.get_analog(ANALOG_LEFT_Y)) > 10){
+        dr_l.move(master.get_analog(ANALOG_LEFT_Y) * 5 * 0.5);
+        dr_r.move(master.get_analog(ANALOG_LEFT_Y) * 5 * 0.5);
+      } else if(master.get_digital(DIGITAL_L1)){
+        dr_r.move_voltage(MAX_MOTOR_VOLTAGE * 0.5);
+        dr_l.move(-MAX_MOTOR_VOLTAGE * 0.5);
+      } else if (master.get_digital(DIGITAL_R1)){
+        dr_l.move(127 * 0.5);
+        dr_r.move(-127 * 0.5);
+      } else {
+        dr_r.move(0);
+        dr_l.move(0);
+      }
 
-    //pushing mechanism
-    if (master.get_digital(DIGITAL_R2)) {
-      push.move(80);
-    } else if (master.get_digital(DIGITAL_L2)) {
-      push.move(-100);
+      //arms
+      arm_l.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10 * 0.5);
+      arm_r.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10 * 0.5);
+
+      //pushing mechanism
+      if (master.get_digital(DIGITAL_R2)) {
+        push.move(80 * 0.5);
+      } else if (master.get_digital(DIGITAL_L2)) {
+        push.move(-100 * 0.5);
+      } else {
+        push.move_velocity(0);
+      }
+
+      //rollers
+      if(master.get_digital(DIGITAL_A)){// spin in
+        roller_left.move(127 * 0.5);
+        roller_right.move(127 * 0.5);
+      } else if(master.get_digital(DIGITAL_B)){ // spin out
+        roller_left.move((3 * -127)/4 * 0.5);
+        roller_right.move((3 * -127)/4 * 0.5);
+      } else { // STOP! in the name of the law!
+        roller_right.move(0);
+        roller_left.move(0);
+      }
+
+// ----------------------------------------------------------------------------------
+
     } else {
-      push.move_velocity(0);
-    }
+      //driving
+      if(abs(master.get_analog(ANALOG_LEFT_Y)) > 10){
+        dr_l.move(master.get_analog(ANALOG_LEFT_Y) * 5);
+        dr_r.move(master.get_analog(ANALOG_LEFT_Y) * 5);
+      } else if(master.get_digital(DIGITAL_L1)){
+        dr_r.move_voltage(MAX_MOTOR_VOLTAGE);
+        dr_l.move(-MAX_MOTOR_VOLTAGE);
+      } else if (master.get_digital(DIGITAL_R1)){
+        dr_l.move(127);
+        dr_r.move(-127);
+      } else {
+        dr_r.move(0);
+        dr_l.move(0);
+      }
 
-    //rollers
-    if(master.get_digital(DIGITAL_A)){// spin in
-      roller_left.move(127);
-      roller_right.move(127);
-    } else if(master.get_digital(DIGITAL_B)){ // spin out
-      roller_left.move((3 * -127)/4);
-      roller_right.move((3 * -127)/4);
-    } else { // STOP! in the name of the law!
-      roller_right.move(0);
-      roller_left.move(0);
+      //arms
+      arm_l.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
+      arm_r.move((master.get_analog(ANALOG_RIGHT_Y) * 8)/10);
+
+      //pushing mechanism
+      if (master.get_digital(DIGITAL_R2)) {
+        push.move(80);
+      } else if (master.get_digital(DIGITAL_L2)) {
+        push.move(-100);
+      } else {
+        push.move_velocity(0);
+      }
+
+      //rollers
+      if(master.get_digital(DIGITAL_A)){// spin in
+        roller_left.move(127);
+        roller_right.move(127);
+      } else if(master.get_digital(DIGITAL_B)){ // spin out
+        roller_left.move((3 * -127)/4);
+        roller_right.move((3 * -127)/4);
+      } else { // STOP! in the name of the law!
+        roller_right.move(0);
+        roller_left.move(0);
+      }
     }
     pros::delay(20);
   }
