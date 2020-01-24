03354a8c52d216fb9436d970e0fd79067b471152
Rahul S. Deshpande <rahulsachindeshpande@gmail.com>
2020-01-24 01:16:23 +0000

    reverse both motors.
    the Motor constructor takes an optional parameter to specify whether the Motor in question is reversed. See: https://pros.cs.purdue.edu/v5/api/cpp/motors.html#constructor-s for more information.

--- src/motors.cpp
+++ src/motors.cpp
@@ -5,5 +5,5 @@ pros::Motor dr_l(abs(L_DR), /* reversed */ true);
 pros::Motor dr_r(R_DR);
 pros::Motor roller_left(L_ROLL);
 pros::Motor roller_right(abs(R_ROLL), /* reversed */ true);
-pros::Motor arm_l(L_ARM);
-pros::Motor arm_r(R_ARM, /* reversed */ true);
+pros::Motor arm_l(L_ARM, /* reversed*/ true);
+pros::Motor arm_r(R_ARM);
