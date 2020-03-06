ac80a76516ec46d514d728dc82bd7447384bdfef
Rahul Deshpande <rahulsachindeshpande@gmail.com>
2020-03-06 15:53:13 -0800

    working 5 point blue autonomous

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,65 +1,3 @@
-/*
-#include "main.hpp"
-#include "motors.hpp"
-#include "okapi/api.hpp"
-
-using namespace okapi;
-
-// figure out how to change motor unit to degree + calculate motor rotations to
-// get to the right spot for the arms. that should make the arms go to the right
-// spot every time; call arms_out() with this position as a param.
-
-void arms_out()
-{
-  arm_l.move(127);
-  arm_r.move(127);
-  pros::delay(1200);
-  arm_r.move(0);
-  arm_l.move(0);
-  pros::delay(200);
-}
-
-void autonomous()
-{
-  //local declaration of drive train
-  auto drive = ChassisControllerBuilder()
-   .withMotors(L_DR, R_DR)
-   // Green gearset, 4 in wheel diam, 11.5 in wheel track
-   .withDimensions(AbstractMotor::gearset::red, {{4_in, 15.5_in}, imev5GreenTPR})
-   .build();
-
-  // bring out arms
-  arms_out();
-  // run rollers
-  roller_left.move(127);
-  roller_right.move(127);
-  //drive forward and pick up line of 4 + one on middle line
-  drive->setMaxVelocity(100);
-  drive->moveDistance(65_in);
-  drive->waitUntilSettled();
-  //drive backwards and then turn + pick up cube by tower
-  drive->moveDistance(-5_in);
-  drive->waitUntilSettled();
-  drive->turnAngle(-45_deg);
-  drive->waitUntilSettled();
-  drive->moveDistance(10_in);
-  drive->waitUntilSettled();
-  //turn around all the way and drive to goal
-  drive->turnAngle(180_deg);
-  drive->moveDistance(63_in);
-  drive->waitUntilSettled();
-  //push forward
-  push.move_voltage(MAX_MOTOR_VOLTAGE);
-  pros::delay(4000);
-  push.move(0);
-  pros::delay(100);
-  //roll out and drive backwards out of goal
-  roller_left.move_velocity(-200);
-  roller_right.move_velocity(200);
-  drive->moveDistanceAsync(-10_in);
-}
-*/
-
 #include "main.hpp"
 #include "motors.hpp"
 #include "okapi/api.hpp"
@@ -71,6 +9,7 @@ figure out how to change motor unit to degree + calculate motor rotations to get
 to the right spot for the arms. that should make the arms go to the right spot
 every time; call with this position as a param.
 */
+
   void autonomous(){
   //local declaration of drive train
   auto drive = ChassisControllerBuilder()
@@ -108,8 +47,8 @@ every time; call with this position as a param.
   drive->waitUntilSettled();
   //move forward to goal
   drive->setMaxVelocity(200);
-  dr_l.move(MAX_MOTOR_POWER);
-  dr_r.move(MAX_MOTOR_POWER);
+  dr_l.move_voltage(MAX_MOTOR_VOLTAGE);
+  dr_r.move_voltage(MAX_MOTOR_VOLTAGE);
   pros::delay(1100);
   dr_l.move(0);
   dr_r.move(0);
